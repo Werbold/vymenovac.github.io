204a3e48bc5c6935236548ebe0018f72d4e08e30
Werbold <matdem1@seznam.cz>
2020-04-29 15:37:11 +0200

    Create Kontakt.php

---
+++ Kontakt.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//CS"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	  <meta charset="UTF-8">
+    <title>Výměna</title>
+<link rel="stylesheet" href="style.css" type="text/css"/link>
+<meta name="description" content="Kontakt na člověka, který vymění cokoliv."/>
+<meta name="keywords" content=" kontakt, formulář, vyměň, výměna, odznak, klopa, předměty, Čechy, Vyměňovač"/>
+<meta name="author" content="Vyměňovač"/>
+
+</head>
+<body>
+<header>
+<nav>
+<ul>
+<li><a href="Hlavni_stranka.html">Informace</a></li>
+<li><a href="1_nabidka.html">Odznak - Buď fit I</a></li>
+<li><a href="2_nabidka.html">Odznak - Buď fit III </a></li>
+<li><a href="3_nabidka.html">Odznak - Česká spořitelna</a></li>
+<li class="aktivni"><a href="Kontakt.php">Kontakt</a></li>
+</ul>
+</nav>
+</header>
+
+<article>
+<div id="centrovac">
+<header>
+<h1>Kontakt</h1>
+</br> </br>
+<p class="centrovany"><img src="obrazky/slogany/2pul_slogan.jpg" width="85%"/img></p>
+</header>
+
+<section>
+
+
+<div class="contact-form">
+<form id="contact-form" action="send.php" method="POST">
+<input name="jmeno" type="text" class="form-control" placeholder="Vaše jméno"/>
+<input name="email" type="email" class="form-control" placeholder="Váš e-mail"/
+>
+</br>
+<textarea name="zprava" type="text" class="form-control" placeholder="Vaše zpráva" row="7" ></textarea></br>
+</br>
+<input type="submit" class="form-control submit" value="Odeslat"/>
+
+
+</form>
+</div>
+
+</section>
+
+<div class="cistic"></div>
+</div>
+</article>
+
+
+<footer>
+<p>Vlastníkem je Vyměňovač</br>
+<em class="mail">vymenovac@post.cz</em></br>
+Made by <em class="mail">matdem2@seznam.cz</em></br>
+Copyright © všechna práva vyhrazena </br></p>
+<p class="paticka">Publikováno X.4. 2020</p>
+</footer>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
